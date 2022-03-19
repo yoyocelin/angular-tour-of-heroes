21594de2f97545a28a9d35bf8e77bfefb0fdd8bf
Yocelin <yoselin_ros@hotmail.com>
2022-03-19 00:30:16 -0400

    initial navigation and creation of dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
 
-@NgModule({
+@NgModule({//this initialize the rooter and listens for browser location changes
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
